720c4e2f5971801a8ba921cad829e3914bd41a76
Ayanshsg19 <ayanshsg19@gmail.com>
2022-10-18 09:57:22 +0530

    bug fixed

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,8 +1,10 @@
+// bug fixed
+
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+mongoose.connect( 'mongodb://127.0.0.1:27017/task-manager-api', {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false
+    useFindAndModify: false,
 })
 
